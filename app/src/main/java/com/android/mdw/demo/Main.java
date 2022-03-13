ed5619cfd6ec179141485395fc4ca8eb87941ebc
Montse <msendinv@gmail.com>
2022-03-13 18:45:18 +0100

    build.gradle updating

--- app/src/main/java/com/android/mdw/demo/Main.java
+++ app/src/main/java/com/android/mdw/demo/Main.java
@@ -1,5 +1,6 @@
 package com.android.mdw.demo;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,27 +13,28 @@ public class Main extends Activity implements OnClickListener {
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.main);
 
-    Button btnInicio = (Button) findViewById(R.id.btnInicio);
-    Button btnFin = (Button) findViewById(R.id.btnFin);
+      Button btnInicio = findViewById(R.id.btnInicio);
+      Button btnFin = findViewById(R.id.btnFin);
 
-    btnInicio.setOnClickListener(this);
-    btnFin.setOnClickListener(this);
+      btnInicio.setOnClickListener(this);
+      btnFin.setOnClickListener(this);
 
-    in = new Intent(this, ElServicio.class);
+      in = new Intent(this, ElServicio.class);
   }
 
+  @SuppressLint("NonConstantResourceId")
   @Override
   public void onClick(View src) {
     switch (src.getId()) {
-    case R.id.btnInicio:      
-      startService(in);
-      break;
-    case R.id.btnFin:
-      stopService(in);
-      break;
+       case R.id.btnInicio:
+          startService(in);
+          break;
+       case R.id.btnFin:
+          stopService(in);
+          break;
     }
   }
 }
